581f6e65f43a45a1a9a1a0d1b9d2e9767e880eee
Tomáš Kaňuch <tomas.kanuch@gmail.com>
2025-03-21 14:27:56 +0100

    feat: Month names

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ def get_day_name():
     date_obj = datetime.utcnow()
     day_name = date_obj.strftime('%A')
     return day_name
+
+
+def get_month_name():
+    date_obj = datetime.utcnow()
+    month_name = date_obj.strftime('%m')
+    return month_name
